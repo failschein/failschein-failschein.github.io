c594bad82f9c2d9c8d72a3cadaa9d8f7e1ea2ff3
Anon <nobody@nowhere.at>
2016-08-17 02:38:32 +0200

    renamed file

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pandoc --smart --toc --normalize -f markdown_github+footnotes+fenced_code_attributes+simple_tables+markdown_in_html_blocks --highlight-style=espresso --template=template.html index.md -o index.html
+pandoc --smart --toc --normalize -f markdown_github+footnotes+fenced_code_attributes+simple_tables+markdown_in_html_blocks --highlight-style=espresso --template=template.html readme.md -o index.html
